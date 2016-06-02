ab60f1a1b737eae2b149f996ca2d236b871e2f6f
ahdpeeb <ahdpeeb.h@gmail.com>
2016-06-02 20:52:52 +0300

    TESTING

--- Objective-C/Objective-C/main.m
+++ Objective-C/Objective-C/main.m
@@ -8,10 +8,42 @@
 
 #import <Foundation/Foundation.h>
 
+#import "ANSCreatureFemale.h"
+#import "ANSCreatureMale.h"
+#import "NSObject+ANSObjectExtension.h"
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        NSMutableArray *creaturesArray = [NSMutableArray autoreleseObject];
+        for (int index = 0; index < count; index ++) {
+            NSString *name = [NSString stringWithFormat:@"Human %d", index];
+            ANSCreature *creature = [[ANSCreature alloc] initWithName:name];
+            creature.gender = [ANSRandom randomIntegerBetweenMinValue:1 maxValue:2];
+            [creaturesArray addObject: creature];
+            
+            [creature dealloc];
+        }
+        
+        for (int index = 0; index < creaturesArray.count; index ++) {
+            ANSCreature *creature = [creaturesArray objectAtIndex:index];
+            
+            ANSCreature *child1 = [ANSCreature autoreleseObject];
+            [creature addChild:child1];
+            
+            [child1 dealloc];
+            
+            ANSCreature *child2 = [ANSCreature autoreleseObject];
+            [creature addChild:child2];
+            
+            [child2 dealloc];
+            
+            if (creature.gender == ANSCreatureGenderMale) {
+                [creature goFight];
+            } else {
+                [creature giveBirth];
+            }
+        }
     }
+    
     return 0;
 }
