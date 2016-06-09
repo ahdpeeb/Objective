235498d078cb691a048c875f00aca5a30b0bdcf5
ahdpeeb <ahdpeeb.h@gmail.com>
2016-06-09 17:48:58 +0300

    NSString+ANSExtension => randomStrnigWithLength created

--- Library/NSString/NSString+ANSExtension.h
+++ Library/NSString/NSString+ANSExtension.h
@@ -10,4 +10,6 @@
 
 @interface NSString (ANSExtension)
 
++ (instancetype)randomStrnigWithLength:(NSInteger)length;
+
 @end
--- Library/NSString/NSString+ANSExtension.m
+++ Library/NSString/NSString+ANSExtension.m
@@ -8,6 +8,17 @@
 
 #import "NSString+ANSExtension.h"
 
+static NSString * const kANSalphabet = @"ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
 @implementation NSString (ANSExtension)
+
++ (instancetype)randomStrnigWithLength:(NSInteger)length {
+    NSMutableString *string = [NSMutableString stringWithCapacity:length];
+    for (NSInteger index = 0; index < length; index ++) {
+        [string appendFormat:@"%C", [kANSalphabet characterAtIndex:arc4random_uniform((u_int32_t)(length))]];
+    }
+    
+    return [self stringWithString:string];
+}
 
 @end
