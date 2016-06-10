07dc54bc5c2657b83a4cf1aecf17264e712557e1
ahdpeeb <ahdpeeb.h@gmail.com>
2016-06-10 19:23:03 +0300

    syntax correction

--- Source/ANSCarWash/ANSConstants.h
+++ Source/ANSCarWash/ANSConstants.h
@@ -6,12 +6,7 @@
 //  Copyright © 2016 Anfriiev.Mykola. All rights reserved.
 //
 
-#ifndef Header_h
-#define Header_h
-
 static const float kANSServiceCost = 5;
 static NSString * const kANSallBoxesFul = @"No sutable box for car";
 static const NSInteger kANSMaxCarCapacity = 1;
 static const NSInteger kANSMaxCarWasherCapacity = 1;
-
-#endif /* Header_h */
