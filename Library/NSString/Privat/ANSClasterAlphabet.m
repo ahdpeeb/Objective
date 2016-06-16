0addbc490564cdb9b5ed48b4a23e96c03d9d4703
ahdpeeb <ahdpeeb.h@gmail.com>
2016-06-16 15:18:00 +0300

    remove ANSClasterAlphabet implementation

--- Library/NSString/Privat/ANSClasterAlphabet.m
+++ Library/NSString/Privat/ANSClasterAlphabet.m
@@ -9,10 +9,7 @@
 #import "ANSClasterAlphabet.h"
 
 @interface ANSClasterAlphabet ()
-@property (nonatomic, retain) NSArray *alphabets;
-@property (nonatomic, assign) NSUInteger count;
 
-- (NSUInteger)countWithAlphabets:(NSArray *)alphabets;
 
 @end
 @implementation ANSClasterAlphabet
@@ -20,42 +17,9 @@
 #pragma mark -
 #pragma mark Initialization and deallocation
 
-- (void)dealloc {
-    self.alphabets = nil;
-    
-    [super dealloc];
-}
-
-- (instancetype)initWithAlphabets:(NSArray *)alphabets {
-    self = [super init];
-    if (self) {
-        self.alphabets = alphabets;
-        self.count = [self countWithAlphabets:alphabets];
-    }
-    
-    return self;
-}
-
 #pragma mark -
 #pragma mark Public
 
-- (NSString *)stringAtIndex:(NSInteger)index {
-    NSUInteger count = self.count;
-    NSUInteger iteretedIndex = index;
-    
-    NSAssert(index < count, NSRangeException);
-    for (ANSAlphabet *alphabet in self.alphabets) {
-        count = [alphabet count];
-        
-        if (iteretedIndex < index) {
-            return ((NSArray *)alphabet)[iteretedIndex];
-        }
-        
-        iteretedIndex -= count;
-    }
-    
-    return nil;
-}
 
 - (NSUInteger)countByEnumeratingWithState:(NSFastEnumerationState *)state
                                   objects:(id [])buffer
@@ -68,14 +32,5 @@
 
 #pragma mark -
 #pragma mark Private
-
-- (NSUInteger)countWithAlphabets:(NSArray *)alphabets {
-    NSInteger count = 0;
-    for (ANSAlphabet *alphabet in self.alphabets) {
-        count += [alphabet count];
-    }
-    
-    return count;
-}
 
 @end
